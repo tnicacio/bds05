329b9d3219bbdbecfe6a98a1c271f66f6773e7ad
tnicacio <nicacio.t92@gmail.com>
2021-07-03 19:27:37 -0300

    Movie Entity Implementation

--- src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -1,16 +1,23 @@
 package com.devsuperior.movieflix.entities;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_movie")
-public class Movie {
+public class Movie implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,11 +26,16 @@ public class Movie {
 	private String subTitle;
 	private Integer year;
 	private String imgUrl;
+	
+	@Column(columnDefinition="TEXT")
 	private String synopsis;
 	
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
 	private Genre genre;
+	
+	@OneToMany(mappedBy = "movie")
+	private final List<Review> reviews = new ArrayList<>();
 	
 	public Movie() {}
 
@@ -91,6 +103,10 @@ public class Movie {
 
 	public void setGenre(Genre genre) {
 		this.genre = genre;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
 	}
 
 	@Override
